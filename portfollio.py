53ba246660c2191289c89544db5dbdf401244aba
Muhammad Musa Maqsood <mmaqsoodulhaq@gmail.com>
2025-05-19 19:55:19 +0500

    Update portfollio.py

--- portfollio.py
+++ portfollio.py
@@ -12,8 +12,8 @@ import requests  # pip install requests
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "Muhammad Musa.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
+
+profile_pic = current_dir / "assets" / "boss.png"
 
 
 # --- GENERAL SETTINGS ---
@@ -23,13 +23,8 @@ NAME = "Muhammad Musa Maqsood"
 DESCRIPTION = """
 Data Analyst, Electrical Engineer
 """
-EMAIL = "mmaqsoodulhaq@gmail.com"
-SOCIAL_MEDIA = {
-    "insta": "https://www.instagram.com/musamaqsood.1/",
-    "facebook": "https://www.facebook.com/musa.maqsood.1",
-    "linkedin":"https://www.linkedin.com/in/muhammad-musa-maqsood-148161297/",
-    
-}
+
+
   
 
 
@@ -39,8 +34,7 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
+
 profile_pic = Image.open(profile_pic)
 
 
@@ -52,13 +46,7 @@ with col1:
 with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
-    st.download_button(
-        label=" 📄 Download Resume",
-        data=PDFbyte,
-        file_name=resume_file.name,
-        mime="application/octet-stream",
-    )
-    st.write("📫", EMAIL)
+   
 
 
 # --- SOCIAL LINKS ---
